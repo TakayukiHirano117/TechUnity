eebd591de015ffabc23e97dd3d2194c01bdc9fc0
TakayukiHirano117 <ikusa.hirano@icloud.com>
2024-12-29 21:45:47 +0900

    ♻️ ✨ 募集一覧ページの見た目部分の実装。募集のコンポーネントへの分離。

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Header from "@/components/organisms/header/Header";
 export default function Home() {
 	return (
 		<div className="">
-			{/* <Header /> */}
+			TOPページ
 		</div>
 	);
 }
--- src/components/organisms/header/Header.tsx
+++ src/components/organisms/header/Header.tsx
@@ -17,58 +17,60 @@ const Header = () => {
 	console.log(session);
 
 	return (
-		<header className="container mx-auto lg:px-20 py-3">
-			<div className="flex justify-between items-center">
-				<div>
-					<Link href="/">
-						{/* svgにしたいな、WIX課金か？ */}
-						<Image src="/Preview.png" alt="logo" width={100} height={100} />
-					</Link>
-				</div>
-				<div className="flex items-center gap-4">
-					<Link href={"/search"}>
-						<SearchIcon className="hover:opacity-70 w-6 h-6 font-bold cursor-pointer" />
-					</Link>
-					{session ? (
-						<div className="flex items-center gap-4">
-							<AvatarIcon
-								className="cursor-pointer"
-								// 画像がundefinedの場合にダミー画像を出す
-								ImageSrc={session.user.image!}
-								fallbackText={session.user.name!}
-							/>
-							<MainButton className="rounded-full font-bold">
-								募集する
-							</MainButton>
-						</div>
-					) : (
-						<MainDialog
-							title="TechUnity"
-							description="TechUnityはチーム開発メンバーの募集をお手伝いする、チーム開発メンバー募集プラットフォームです。"
-							trigger={
+		<header className="border-b">
+			<div className="container mx-auto lg:px-20 py-3">
+				<div className="flex justify-between items-center">
+					<div>
+						<Link href="/">
+							{/* svgにしたいな、WIX課金か？ */}
+							<Image src="/Preview.png" alt="logo" width={100} height={100} />
+						</Link>
+					</div>
+					<div className="flex items-center gap-4">
+						<Link href={"/search"}>
+							<SearchIcon className="hover:opacity-70 w-6 h-6 font-bold cursor-pointer" />
+						</Link>
+						{session ? (
+							<div className="flex items-center gap-4">
+								<AvatarIcon
+									className="cursor-pointer"
+									// 画像がundefinedの場合にダミー画像を出す
+									ImageSrc={session.user.image!}
+									fallbackText={session.user.name!}
+								/>
 								<MainButton className="rounded-full font-bold">
-									ログイン
+									募集する
 								</MainButton>
-							}
-						>
-							<MainButton
-								className="rounded-full font-bold"
-								variant="outline"
-								onClick={() => signIn("github")}
+							</div>
+						) : (
+							<MainDialog
+								title="TechUnity"
+								description="TechUnityはチーム開発メンバーの募集をお手伝いする、チーム開発メンバー募集プラットフォームです。"
+								trigger={
+									<MainButton className="rounded-full font-bold">
+										ログイン
+									</MainButton>
+								}
 							>
-								<GitHubIcon />
-								GitHubでログイン
-							</MainButton>
-							<MainButton
-								className="rounded-full font-bold"
-								variant="outline"
-								onClick={() => signIn("google")}
-							>
-								<GoogleIcon />
-								Googleでログイン
-							</MainButton>
-						</MainDialog>
-					)}
+								<MainButton
+									className="rounded-full font-bold"
+									variant="outline"
+									onClick={() => signIn("github")}
+								>
+									<GitHubIcon />
+									GitHubでログイン
+								</MainButton>
+								<MainButton
+									className="rounded-full font-bold"
+									variant="outline"
+									onClick={() => signIn("google")}
+								>
+									<GoogleIcon />
+									Googleでログイン
+								</MainButton>
+							</MainDialog>
+						)}
+					</div>
 				</div>
 			</div>
 		</header>
