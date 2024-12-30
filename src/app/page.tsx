1cc3178d6fa7c44bd063c1a4bd1942d595b4c121
TakayukiHirano117 <ikusa.hirano@icloud.com>
2024-12-30 17:59:16 +0900

    ✨ 募集作成フォームの作成

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,3 @@
-import Header from "@/components/organisms/header/Header";
-
 export default function Home() {
-	return (
-		<div className="">
-			TOPページ
-		</div>
-	);
+	return <div className="">TOPページ</div>;
 }
