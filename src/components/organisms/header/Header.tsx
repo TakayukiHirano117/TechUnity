3c4fd8c0581124556fbb5d45d0ca1a99a538b607
TakayukiHirano117 <ikusa.hirano@icloud.com>
2025-01-13 21:46:15 +0900

    ✨ Credentials認証を導入

--- src/components/organisms/header/Header.tsx
+++ src/components/organisms/header/Header.tsx
@@ -1,26 +1,68 @@
 "use client";
 
+import { zodResolver } from "@hookform/resolvers/zod";
 import Image from "next/image";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 import { signIn, useSession } from "next-auth/react";
 import React, { memo } from "react";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+import { z } from "zod";
 
 import AvatarIcon from "@/components/atoms/avatar/AvatarIcon";
 import MainButton from "@/components/atoms/button/MainButton";
 import GitHubIcon from "@/components/atoms/Icon/GitHubIcon";
 import GoogleIcon from "@/components/atoms/Icon/GoogleIcon";
-import SearchIcon from "@/components/atoms/SearchIcon";
 import MainDialog from "@/components/molecules/dialog/MainDialog";
 import MainDropdown from "@/components/molecules/dropdown/MainDropdown";
 import SearchBar from "@/components/molecules/search/SearchBar";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+} from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Skeleton } from "@/components/ui/skeleton";
 
-import HeaderWrapper from "./HeaderWrapper";
+const formSchema = z.object({
+  email: z.string().email(),
+  username: z.string(),
+  password: z.string().min(8),
+});
 
 // propsを受け取ってないのでmemo化する意味はないが今後渡すかもしれないので忘れないうちにとりあえずやっとく。
 const Header: React.FC = memo(() => {
+  const router = useRouter();
+  // DBから取得する方式に変える。
   const { data: session, status } = useSession();
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      username: "",
+      password: "",
+    },
+  });
+
+  const onSubmit: SubmitHandler<FieldValues> = async (data) => {
+    await fetch("/api/auth/signup", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+
+    await signIn("credentials", {
+      ...data,
+      redirect: false,
+    });
+    
+    router.refresh();
+  };
 
   return (
     <header className="border-b px-2">
@@ -36,38 +78,130 @@ const Header: React.FC = memo(() => {
             {status === "loading" ? (
               <Skeleton className="w-40 h-10 rounded-full" />
             ) : !session ? (
-              <MainDialog
-                title="TechUnity"
-                description="TechUnityはチーム開発メンバーの募集をお手伝いする、チーム開発メンバー募集プラットフォームです。"
-                trigger={
-                  <MainButton className="rounded-full font-bold">
-                    ログイン
+              <>
+                <MainDialog
+                  title="TechUnity"
+                  description="TechUnityはチーム開発メンバーの募集をお手伝いする、チーム開発メンバー募集プラットフォームです。"
+                  trigger={
+                    <MainButton className="rounded-full font-bold">
+                      ログイン
+                    </MainButton>
+                  }
+                >
+                  <MainButton
+                    className="rounded-full font-bold"
+                    variant="outline"
+                    onClick={() => signIn("github")}
+                  >
+                    <GitHubIcon />
+                    GitHubでログイン
                   </MainButton>
-                }
-              >
-                <MainButton
-                  className="rounded-full font-bold"
-                  variant="outline"
-                  onClick={() => signIn("github")}
+                  <MainButton
+                    className="rounded-full font-bold"
+                    variant="outline"
+                    onClick={() => signIn("google")}
+                  >
+                    <GoogleIcon />
+                    Googleでログイン
+                  </MainButton>
+                </MainDialog>
+                <MainDialog
+                  title="TechUnity"
+                  description="TechUnityはチーム開発メンバーの募集をお手伝いする、チーム開発メンバー募集プラットフォームです。"
+                  trigger={
+                    <MainButton
+                      className="rounded-full font-bold"
+                      variant={"outline"}
+                    >
+                      新規登録
+                    </MainButton>
+                  }
                 >
-                  <GitHubIcon />
-                  GitHubでログイン
-                </MainButton>
-                <MainButton
-                  className="rounded-full font-bold"
-                  variant="outline"
-                  onClick={() => signIn("google")}
-                >
-                  <GoogleIcon />
-                  Googleでログイン
-                </MainButton>
-              </MainDialog>
+                  <h3 className="text-center font-bold text-2xl">
+                    メールアドレスで登録
+                  </h3>
+                  <Form {...form}>
+                    <form
+                      onSubmit={form.handleSubmit(onSubmit)}
+                      className="flex flex-col gap-2"
+                    >
+                      <FormField
+                        control={form.control}
+                        name="email"
+                        render={({ field }) => (
+                          <FormItem>
+                            <FormLabel>メールアドレス</FormLabel>
+                            <FormControl>
+                              <Input
+                                placeholder="aaa@example.com"
+                                type="email"
+                                {...field}
+                              />
+                            </FormControl>
+                          </FormItem>
+                        )}
+                      />
+                      <FormField
+                        control={form.control}
+                        name="username"
+                        render={({ field }) => (
+                          <FormItem>
+                            <FormLabel>ユーザー名</FormLabel>
+                            <FormControl>
+                              <Input
+                                placeholder="Taro Yamada"
+                                type="text"
+                                {...field}
+                              />
+                            </FormControl>
+                          </FormItem>
+                        )}
+                      />
+                      <FormField
+                        control={form.control}
+                        name="password"
+                        render={({ field }) => (
+                          <FormItem>
+                            <FormLabel>パスワード</FormLabel>
+                            <FormControl>
+                              <Input type="password" {...field} />
+                            </FormControl>
+                          </FormItem>
+                        )}
+                      />
+                      <MainButton
+                        type="submit"
+                        className="rounded-full font-bold"
+                      >
+                        新規登録する
+                      </MainButton>
+                    </form>
+                  </Form>
+                  <p className="text-sm text-slate-600 text-center">または</p>
+                  <MainButton
+                    className="rounded-full font-bold"
+                    variant="outline"
+                    onClick={() => signIn("github")}
+                  >
+                    <GitHubIcon />
+                    GitHubで登録
+                  </MainButton>
+                  <MainButton
+                    className="rounded-full font-bold"
+                    variant="outline"
+                    onClick={() => signIn("google")}
+                  >
+                    <GoogleIcon />
+                    Googleで登録
+                  </MainButton>
+                </MainDialog>
+              </>
             ) : (
               <div className="flex items-center gap-4">
                 <MainDropdown username={session.user.name!}>
                   <button>
                     <AvatarIcon
-                      className="cursor-pointer"
+                      className="cursor-pointer border"
                       ImageSrc={session.user.image!}
                       fallbackText={session.user.name!}
                     />
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,47 +1,84 @@
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcrypt";
 import { NextAuthOptions } from "next-auth";
+import Credentials from "next-auth/providers/credentials";
 import Github from "next-auth/providers/github";
 import Google from "next-auth/providers/google";
 
 const prisma = new PrismaClient();
 
 export const authOptions: NextAuthOptions = {
-	providers: [
-		Github({
-			clientId: process.env.GITHUB_CLIENT_ID!,
-			clientSecret: process.env.GITHUB_SECRET!,
-		}),
-		Google({
-			clientId: process.env.GOOGLE_CLIENT_ID!,
-			clientSecret: process.env.GOOGLE_SECRET!,
-		}),
-	],
-	adapter: PrismaAdapter(prisma),
-	pages: {
-		// signIn後のリダイレクト先を設定
-		signIn: "/",
-		signOut: "/",
-	},
-	callbacks: {
-		async jwt({ token, user }) {
-			if (user) {
-				return { ...token, id: user.id };
-			}
+  providers: [
+    Github({
+      clientId: process.env.GITHUB_CLIENT_ID!,
+      clientSecret: process.env.GITHUB_SECRET!,
+    }),
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_SECRET!,
+    }),
+    Credentials({
+      name: "credentials",
+      credentials: {
+        email: { label: "Email", type: "text" },
+        username: { label: "Username", type: "text" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password) {
+          throw new Error("メールアドレスとパスワードが存在しません");
+        }
 
-			return token;
-		},
-		async session({ token, session }) {
-			if (token) {
-				session.user.id = token.id;
-				session.user.name = token.name;
-				session.user.email = token.email;
-				session.user.image = token.picture;
-			}
-			return session;
-		},
-	},
-	session: {
-		strategy: "jwt",
-	},
+        const user = await prisma.user.findUnique({
+          where: {
+            email: credentials?.email,
+            // password: credentials.password,
+          },
+        });
+
+        if (!user || !user?.hashedPassword) {
+          throw new Error("ユーザーが存在しません");
+        }
+
+        const isCorrectPassword = await bcrypt.compare(
+          credentials.password,
+          user.hashedPassword,
+        );
+
+        if (!isCorrectPassword) {
+          throw new Error("パスワードが一致しません");
+        }
+
+        return user;
+      },
+    }),
+  ],
+  adapter: PrismaAdapter(prisma),
+  pages: {
+    // signIn後のリダイレクト先を設定
+    signIn: "/",
+    signOut: "/",
+  },
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        return { ...token, id: user.id };
+      }
+
+      return token;
+    },
+    async session({ token, session }) {
+      if (token) {
+        session.user.id = token.id;
+        session.user.name = token.name;
+        session.user.email = token.email;
+        session.user.image = token.picture;
+      }
+      return session;
+    },
+  },
+  session: {
+    strategy: "jwt",
+  },
 };
