e8fbd75910e44ab6acae4ae9e855cd4a05bfbca2
TakayukiHirano117 <ikusa.hirano@icloud.com>
2025-01-18 18:28:31 +0900

    ♻️ fix

--- src/lib/fetcher/profile.ts
+++ src/lib/fetcher/profile.ts
@@ -1,14 +1,21 @@
+import { headers } from "next/headers";
+
 export const getProfile = async () => {
   const res = await fetch(
     "https://tech-unity.vercel.app/api/dashboard/profiles",
+    {
+      // headers情報がないからgetTokenできなかったっぽい。
+      headers: Object.fromEntries(headers()),
+    },
   );
   const profile = await res.json();
   return profile;
 };
 
 export const getRecruitsWithProfile = async (id: string) => {
-  const res = await fetch(
-    `https://tech-unity.vercel.app/api/profiles/${id}`,
-  );
+  const res = await fetch(`https://tech-unity.vercel.app/api/profiles/${id}`, {
+    // headers情報がないからgetTokenできなかったっぽい。
+    headers: Object.fromEntries(headers()),
+  });
   return res.json();
 };
