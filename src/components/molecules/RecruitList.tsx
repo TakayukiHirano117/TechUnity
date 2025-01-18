593a2127a11067b47f8cbbd8e79e8efe9cc5dd44
TakayukiHirano117 <ikusa.hirano@icloud.com>
2025-01-18 17:12:58 +0900

    ♻️ fix

--- src/components/molecules/RecruitList.tsx
+++ src/components/molecules/RecruitList.tsx
@@ -1,47 +1,47 @@
 import { Application, Like } from "@prisma/client";
 import React from "react";
+
 import RecruitCard from "@/components/molecules/card/RecruitCard";
 
 const RecruitList = ({
-	recruits,
+  recruits,
 }: {
-	recruits: {
-		id: number;
-		title: string;
-		content: string;
-		creator: {
-			id: string;
-			name: string;
-			image: string;
-		};
-		createdAt: string;
-		updatedAt: string;
-		isPublished: boolean;
-		likes: Like[];
-		applications: Application[];
-		remainingCount: number;
-	}[];
+  recruits: {
+    id: number;
+    title: string;
+    content: string;
+    creator: {
+      id: string;
+      name: string;
+      image: string;
+    };
+    createdAt: string;
+    updatedAt: string;
+    isPublished: boolean;
+    likes: Like[];
+    applications: Application[];
+    remainingCount: number;
+  }[];
 }) => {
-	console.log(recruits)
-	return (
-		<div className="grid grid-cols-1 sm:grid-cols-2 gap-8">
-			{recruits.map((recruit) => (
-				<RecruitCard
-					key={recruit.id}
-					id={recruit.id}
-					title={recruit.title}
-					description={recruit.content}
-					authorName={recruit?.creator.name}
-					authorId={recruit?.creator.id}
-					avatarImageSrc={recruit?.creator.image}
-					publishedAt={recruit.createdAt}
-					likes={recruit.likes}
-					applications={recruit.applications}
-					remainingCount={recruit?.remainingCount}
-				/>
-			))}
-		</div>
-	);
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 gap-8">
+      {recruits.map((recruit) => (
+        <RecruitCard
+          key={recruit.id}
+          id={recruit.id}
+          title={recruit.title}
+          description={recruit.content}
+          authorName={recruit?.creator.name}
+          authorId={recruit?.creator.id}
+          avatarImageSrc={recruit?.creator.image}
+          publishedAt={recruit.createdAt}
+          likes={recruit.likes}
+          applications={recruit.applications}
+          remainingCount={recruit?.remainingCount}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default RecruitList;
