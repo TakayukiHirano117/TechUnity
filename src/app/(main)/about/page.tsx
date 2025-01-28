804dc131e862a0041b99972df4d99b5c22fea2a8
TakayukiHirano117 <ikusa.hirano@icloud.com>
2025-01-28 15:45:38 +0900

    ♻️ aboutページのUIを調整

--- src/app/(main)/about/page.tsx
+++ src/app/(main)/about/page.tsx
@@ -4,15 +4,15 @@ import React from "react";
 // 未実装です
 const AboutPage = () => {
   return (
-    <div className="flex flex-col gap-8 justify-center items-center h-screen">
-      <div className="flex gap-2">
+    <div className="px-8 flex flex-col gap-8 justify-center items-center h-screen container mx-auto max-width-[960px]">
+      <div className="flex gap-2 items-center">
         <Image
           src={"/apple-touch-icon.png"}
           height={32}
           width={60}
           alt="logo"
         />
-        <h1 className="font-bold text-2xl sm:text-6xl">TechUnity</h1>
+        <h1 className="font-bold text-3xl sm:text-6xl">TechUnity</h1>
       </div>
       <Image
         src="/undraw_development_s4gv.svg"
