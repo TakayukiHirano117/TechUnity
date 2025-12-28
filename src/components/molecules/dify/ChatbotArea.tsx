4c2c19603c960e528d980933054e8e7192915f8e
TakayukiHirano117 <ikusa.hirano@icloud.com>
2025-12-28 15:38:33 +0900

    vibe: dify入れた

---
+++ src/components/molecules/dify/ChatbotArea.tsx
@@ -0,0 +1,29 @@
+import Script from "next/script";
+
+const DIFY_TOKEN = process.env.NEXT_PUBLIC_DIFY_TOKEN || "oBwt0bauGPyI2IVo";
+
+export default function DifyChatbot() {
+  return (
+    <>
+      <Script
+        id="dify-chatbot-config"
+        strategy="beforeInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.difyChatbotConfig = {
+              token: '${DIFY_TOKEN}',
+              inputs: {},
+              systemVariables: {},
+              userVariables: {},
+            }
+          `,
+        }}
+      />
+      <Script
+        src="https://udify.app/embed.min.js"
+        id={DIFY_TOKEN}
+        strategy="beforeInteractive"
+      />
+    </>
+  );
+}
