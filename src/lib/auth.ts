97ad854ee762d5eaef65b04a4a48402971f4e989
TakayukiHirano117 <ikusa.hirano@icloud.com>
2026-01-01 11:53:54 +0900

    commit

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,5 +1,5 @@
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import { NextAuthOptions } from "next-auth";
 import Credentials from "next-auth/providers/credentials";
 import Github from "next-auth/providers/github";
